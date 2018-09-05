94475cbd850676b9b552a439f9b8497af84ed4a5
zousir <1419107017@qq.com>
2018-09-05 22:56:20 +0800

    add

---
+++ pages/home/home-model.js
@@ -0,0 +1,25 @@
+import {Base} from '../../utils/base.js';
+class Home extends Base{
+  constructor(){
+    super();
+  }
+  getBannerData(id, callBack){
+    var params = {
+      url: 'banner/'+id,
+      sCallBack:function(res){
+        callBack && callBack(res.items);
+      }
+    }
+    this.request(params);
+    // wx.request({
+    //   url: 'http://z.cn/index.php/api/v1/banner/'+id,
+    //   method: 'GET',
+    //   success: function(res){
+    //     // console.log(res);
+    //     // return res;
+    //     callBack(res);
+    //   } 
+    // })
+  }
+}
+export {Home};
---
+++ utils/config.js
@@ -0,0 +1,7 @@
+class Config{
+  constructor(){
+
+  }
+}
+Config.restUrl = 'http://z.cn/index.php/api/v1/';
+export {Config};
